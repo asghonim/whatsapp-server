1d28e0ab4aca229d02957cfe8d47f6d6cbaba070
Ghonim <asghonim@live.com>
2026-06-03 16:07:07 +0000

    Init

---
+++ .devcontainer/init.sh
@@ -0,0 +1,2 @@
+# The following line is needed to ensure that the "xdg-open" command is available in the container, which is required for opening links in the default browser.
+[ -f "$BROWSER" ] && ! command -v xdg-open > /dev/null && sudo ln -s "$BROWSER" /usr/local/bin/xdg-open
